b0d3924f44ebadf70f1e44de96396864b34f86f3
PLWEP <permana.putra@ifssi.co.id>
2026-09-25 16:27:32 +0700

    fix(network): add android internet permission and resolve internal enterprise host mapping

--- lib/core/network/auth_interceptor.dart
+++ lib/core/network/auth_interceptor.dart
@@ -5,11 +5,25 @@ import 'api_config.dart';
 class AuthInterceptor extends Interceptor {
   final ApiConfig _config = ApiConfig.instance;
 
+  static const Map<String, String> _internalHosts = {
+    'isidemocloud.ifssi.co.id': '10.53.25.132',
+    'isidemobtg.ifssi.co.id': '10.53.25.51',
+    'csd-cloud.ifssi.co.id': '10.53.25.148',
+    'app-demo.ifssi.co.id': '10.53.25.148',
+  };
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     options.headers['Accept'] = 'application/json';
     if (options.contentType == null && !options.headers.containsKey('Content-Type')) {
       options.headers['Content-Type'] = 'application/json;charset=utf-8';
+    }
+
+    final uri = options.uri;
+    final mappedIp = _internalHosts[uri.host];
+    if (mappedIp != null) {
+      options.headers['Host'] = uri.host;
+      options.path = uri.replace(host: mappedIp).toString();
     }
 
     if (_config.isAuthenticated) {
